b9433abd644fb87a0bd9156aa4e4c8cdbd0872fe
Satya Mahesh <satyamahesh.kolli03@gmail.com>
2025-05-24 01:14:52 +0530

    Update blogService.js

--- frontend/src/services/blogService.js
+++ frontend/src/services/blogService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://blogeditor-backend-q4pb.onrender.com/api/blogs';
+const BASE_URL = 'http://localhost:6500/api/blogs';
 
 export const saveDraft = (data) => axios.post(`${BASE_URL}/save-draft`, data);
 export const publishBlog = (data) => axios.post(`${BASE_URL}/publish`, data);
